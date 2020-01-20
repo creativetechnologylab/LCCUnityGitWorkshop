370c8d1b083a3c0fe57fe9f0b99e550aeaf23086
Absolist <ashghane@hotmail.com>
2020-01-20 15:37:10 +0000

    spinning man

---
+++ Assets/CapsuleMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleMove : MonoBehaviour
+{
+    public GameObject capsule;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        capsule.transform.Rotate(10f, 40f, 0);
+        
+    }
+}
